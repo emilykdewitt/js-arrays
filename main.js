222a5ba61864149ebee3a8024b2e4c339ebbf3e6
Emily DeWitt <emilykdewitt@gmail.com>
2019-03-05 21:21:53 -0600

    Added assignment cards and corresponding css

--- main.js
+++ main.js
@@ -31,10 +31,68 @@ const buildDinosaurs = () => {
     printToDom('dino-barn', domString);
 };
 
+const assignments = [
+    {
+        title: 'product cards',
+        dueDate: '03/05/2019',
+        topic: 'HTML/CSS',
+        notes: 'use flexbox',
+        assigmentURL: 'www.google.com'
+    },
+    {
+        title: 'monkeys',
+        dueDate: '04/01/2019',
+        topic: 'JavaScript',
+        notes: 'use wizard hat',
+        assignmentURL: 'www.wizards.com'
+    },
+    {
+        title: 'kittens',
+        dueDate: '05/12/2019',
+        topic: 'Python',
+        notes: 'use magic wand',
+        assignmentURL: 'www.pixies.com'
+    },
+    {
+        title: 'antelopes',
+        dueDate: '06/02/2019',
+        topic: 'JavaScript',
+        notes: 'use endless handkerchief',
+        assignmentURL: 'www.mermaids.com'
+    },
+    {
+        title: 'zebras',
+        dueDate: '07/03/2019',
+        topic: 'PHP',
+        notes: 'use invisibility cloak',
+        assignmentURL: 'www.potterheads.com'
+    }
+];
+
+//total of 5 assignments - loop over assignments and make a domstring
+//reuse the printToDom function to display the domString
+
+const buildAssCards = () => {
+    let domString = '';
+    for(let i = 0; i < assignments.length; i++) {
+        domString += `<div class="assignment">`;
+        domString += `<h5 class="assignment-title">${assignments[i].title}</h5>`;
+        domString += `<div class="due-date"><h3>Due Date: ${assignments[i].dueDate}</h3></div>`;
+        domString += `<p>Get the assignment <a href=${assignments[i].assignmentURL}>HERE</a></p>`;
+        domString += `<p>Topic: ${assignments[i].topic}</p>`;
+        domString += `<p>Notes: ${assignments[i].notes}</p>`;
+        domString += `</div>`;
+    }
+    printToDom('assignment-cards', domString);
+};
+
+buildAssCards();
+
 //the initializer below will run anything that needs to load upon page load, 
 //usually for things that don't require user action or input
 const init = () => {
     buildDinosaurs();
+    buildAssCards();
 };
 
 init();
